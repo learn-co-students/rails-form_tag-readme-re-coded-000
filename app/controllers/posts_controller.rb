2ed71ddd23fd694cee6f5b86f6031ca5043261f2
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-12-31 19:23:22 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def new
   end
   def create
-  Post.create(title: params[:post][:title], description: params[:post][:description])
+  Post.create(title: params[:title], description: params[:description])
   redirect_to posts_path
 end
 end
